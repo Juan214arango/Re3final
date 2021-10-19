2bb8ee5d056b20e1ed293a5f2e38b52d464187c7
Juan214arango <68446046+Juan214arango@users.noreply.github.com>
2021-10-19 00:03:52 -0500

    Delete masterclass3 directory

--- masterclass3/src/main/java/co/usa/ciclo3/ciclo3/repository/crud/FarmsCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package co.usa.ciclo3.ciclo3.repository.crud;
-
-import co.usa.ciclo3.ciclo3.model.Farms;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FarmsCrudRepository extends CrudRepository<Farms,Integer> {
-    
-}
